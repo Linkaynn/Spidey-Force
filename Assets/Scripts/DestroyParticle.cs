d0fd540d9345806e2af3f2e7776f636af3113168
Linkaynn <jroarbelo@gmail.com>
2015-03-18 20:08:11 +0100

    1.1v
    Se añaden prefabs de partículas.

---
+++ Assets/Scripts/DestroyParticle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyParticle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("destroy", 2f);
+	}
+	
+	void destroy(){
+		Destroy (this.gameObject);
+	}
+}
